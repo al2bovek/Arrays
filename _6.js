cc3773ed6c15a7b54acf88dc0906d4e9f41a2db4
D2JP <d2jp@TECHIN.INTERNAL>
2025-11-24 12:16:43 +0200

    _6.js

--- _6.js
+++ _6.js
@@ -1,11 +1,20 @@
 "use strict";
 // Given an array of numbers, return their product. Use reduce metod.
 
-const arr = [0, 1, 4, 8, 10];
+const arr = [20, 1, 4, 8, 10];
 const operator = prompt("operator");
-const returnProduct = (arr, operator) => {
-  let result = arr.reduce((ac, el) => ac operator el);
+const returnProduct = (arr, operator = "+") => {
 
+  const result = arr.reduce((ac, el) => {
+    switch (operator) {
+      case "+": return ac + el;
+      case "-": return ac - el;
+      case "*": return ac * el;
+      case "/": return ac / el;
+      // default: throw new Error("Wrong operator");
+      default: return "Wrong operator";
+    }
+  });
   return result;
 }
 document.getElementById("array").innerHTML = returnProduct(arr, operator);
