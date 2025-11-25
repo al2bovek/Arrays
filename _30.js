c025d76966a75f9b3f62e692081d2cadd6eb4690
al2bovek <aleksandr.dubovek@gmail.com>
2025-11-25 21:04:23 +0200

    _30.js

--- _30.js
+++ _30.js
@@ -3,8 +3,12 @@
 
 const names = ['Susan', 'Anthony', 'Bill'];
 const employees = ['Bill', "Fill", "Susana"];
-let result = [];
+
 function recognizeEmployees(names, employees) {
+  const great = [];
+  const outstanding = [];
+  names.map(name => !employees.includes(name) ? great.push(`Great job, ${name}`) : outstanding.push(`Outstanding job, ${name}`));
+  const result = [...great, ...outstanding];
   return result;
 }
 
